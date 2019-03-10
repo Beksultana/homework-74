f939b3cf084abef2b774217f66bb66104ecb068e
sultan <aidaraliuulu@gmail.com>
2019-03-10 19:48:15 +0600

    exported roter

--- app/messages.js
+++ app/messages.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Get id mess')
+    res.send('Get mess')
 });
 
 router.get('/:id', (req, res) => {
@@ -12,3 +12,5 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
     res.send('mess post')
 });
+
+module.exports = router;
